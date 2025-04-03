1f665c236156c2d2190f9edf967e23ecd71aa08c
wenhiii <wenhao20040426zhang@gmail.com>
2025-04-03 14:28:20 +0200

    🎶

--- src/main/java/es/codeurjc/test/ejem/Calculadora.java
+++ src/main/java/es/codeurjc/test/ejem/Calculadora.java
@@ -3,7 +3,7 @@ package es.codeurjc.test.ejem;
 public class Calculadora {
 
 	public double suma(double op1, double op2) {
-		return op1 * op2;
+		return op1 + op2;
 	}
 
 	public double resta(double op1, double op2) {
